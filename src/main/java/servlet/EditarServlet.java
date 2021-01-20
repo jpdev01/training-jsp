25ac1332c01951152188035a81302a2d10ccd1f6
joao.borba <jptruchinski@gmail.com>
2021-01-19 22:15:27 -0300

    Criação do servlet Editar

--- src/main/java/servlet/EditarServlet.java
+++ src/main/java/servlet/EditarServlet.java
@@ -1,4 +1,30 @@
 package servlet;
 
-public class EditarServlet {
+import model.Agenda;
+import model.Contato;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/Contato/Editar/*")
+public class EditarServlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+         Integer id = req.getParameter("id") != null ? Integer.valueOf(req.getParameter("id")) : null;
+         // metodo singleton
+         Agenda agenda = Agenda.getInstance();
+
+         if (id != null){
+             Contato contato = agenda.carregar(id);
+             req.setAttribute("contato", contato);
+         }
+
+         req.getRequestDispatcher("/editar.jsp").forward(req, resp);
+    }
 }
