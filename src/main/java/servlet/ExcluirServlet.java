d88a555f20f0c743a377c8a3635a2fa4953ef750
joao.borba <jptruchinski@gmail.com>
2021-01-19 22:20:00 -0300

    Criação do Servlet de editar

--- src/main/java/servlet/ExcluirServlet.java
+++ src/main/java/servlet/ExcluirServlet.java
@@ -1,4 +1,26 @@
 package servlet;
 
-public class ExcluirServlet {
+import model.Agenda;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/Contato/Excluir/*")
+public class ExcluirServlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (req.getParameter("id") != null){
+            Integer id = Integer.valueOf(req.getParameter("id"));
+
+            Agenda agenda = Agenda.getInstance();
+            agenda.excluir(id);
+        }
+        resp.sendRedirect("/Agenda/Listar");
+    }
 }
