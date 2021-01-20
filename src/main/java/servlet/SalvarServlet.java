bacb74898a5bace9d2208afc44c3975baba23138
joao.borba <jptruchinski@gmail.com>
2021-01-19 22:06:33 -0300

    Criação do servlet Salvar

---
+++ src/main/java/servlet/SalvarServlet.java
@@ -0,0 +1,29 @@
+package servlet;
+
+import model.Agenda;
+import model.Contato;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/Agenda/Salvar/*")
+public class SalvarServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Integer id = req.getParameter("id") != null ? Integer.valueOf(req.getParameter("id")) : null;
+        String name = req.getParameter("name");
+        String phone = req.getParameter("telephone");
+
+        Contato c = new Contato(id, name, phone);
+
+        Agenda agenda = Agenda.getInstance();
+        agenda.salvar(c);
+
+        resp.sendRedirect("/Agenda/Listar");
+    }
+}
