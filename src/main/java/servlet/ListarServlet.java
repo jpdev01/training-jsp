bdc0e697e39a2ebdc5a3f954aa52eb4dd436b12d
joao.borba <jptruchinski@gmail.com>
2021-01-19 22:10:45 -0300

    Criação do Servlet listar

--- src/main/java/servlet/ListarServlet.java
+++ src/main/java/servlet/ListarServlet.java
@@ -1,4 +1,27 @@
 package servlet;
 
-public class ListarServlet {
+import model.Agenda;
+import model.Contato;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Set;
+
+@WebServlet("/Agenda/Listar/*")
+public class ListarServlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Agenda agenda = Agenda.getInstance();
+
+        Set<Contato> contatos = agenda.getContatos();
+        req.setAttribute("contatos", contatos);
+
+        req.getRequestDispatcher("/listar.jsp").forward(req, resp);
+    }
 }
