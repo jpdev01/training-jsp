b7974dd6d8f2ae56202487ba0775c0a3422ffdab
joao.borba <jptruchinski@gmail.com>
2021-01-21 21:36:47 -0300

    servlets devem começar seu endereço com /, e nao terminar

--- src/main/java/servlet/ExcluirServlet.java
+++ src/main/java/servlet/ExcluirServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/Contato/Excluir/*")
+@WebServlet("/agenda-exluir")
 public class ExcluirServlet extends HttpServlet {
 
 
