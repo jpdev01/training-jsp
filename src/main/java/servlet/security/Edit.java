a440a3b01e87de6725ad761eae0628b19215c15a
joao.borba <jptruchinski@gmail.com>
2021-01-24 08:46:40 -0300

    Servlet security/edit -> ok

--- src/main/java/servlet/security/Edit.java
+++ src/main/java/servlet/security/Edit.java
@@ -1,7 +1,26 @@
 package servlet.security;
 
+import model.User;
+import model.Users;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class Edit extends HttpServlet {
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Integer id = (req.getParameter("id") != null) ? Integer.valueOf(req.getParameter("id")) : null;
+
+        Users users = Users.getInstance();
+        if (id != null){
+            User user = users.load(id);
+            req.setAttribute("user", user);
+        }
+
+        req.getRequestDispatcher("security/edit.jsp").forward(req, resp);
+    }
 }
