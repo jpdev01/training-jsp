0c63f25bf07dae285da999ead8e68c83700122fd
joao.borba <jptruchinski@gmail.com>
2021-01-23 21:53:39 -0300

    Lógica de TimeFilter implementada - ok

--- src/main/java/filter/TimeFilter.java
+++ src/main/java/filter/TimeFilter.java
@@ -4,13 +4,19 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
 
-@WebFilter(filterName = "Filter")
+@WebFilter(filterName = "Timer", value = "/*")
 public class TimeFilter implements Filter {
     public void destroy() {
     }
 
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
-        chain.doFilter(req, resp);
+        long inicio = System.currentTimeMillis(); // inicio do cronometro
+
+        chain.doFilter(req, resp); // chama a request
+        // voltando da request
+        long fim = System.currentTimeMillis();
+        long tempoDeProcessamento = (fim - inicio);
+        System.out.println("Tempo de processamento:" + tempoDeProcessamento + " milissegundos");
     }
 
     public void init(FilterConfig config) throws ServletException {
