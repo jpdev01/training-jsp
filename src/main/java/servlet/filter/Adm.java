0505bf88ad15e8b3ed9e5151a5dce10307f167db
João Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-01-30 12:47:38 -0300

    Revert "Adicionando filtro de autenticação no diretório /adm"

--- src/main/java/servlet/filter/Adm.java
+++ src/main/java/servlet/filter/Adm.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name="adm", value="/user/*")
+//@WebServlet(name="adm", value="/sec/adm")
 public class Adm extends HttpServlet {
 
     @Override
