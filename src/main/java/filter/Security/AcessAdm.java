d3da91f03c44fd10b8408ffa57ed437a418199f6
João Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-01-31 14:23:20 -0300

    Correção no atributo userInfo que nunca era apagado

--- src/main/java/filter/Security/AcessAdm.java
+++ src/main/java/filter/Security/AcessAdm.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebFilter(value="/sec/*")
+@WebFilter(value="/portal/sec/*")
 public class AcessAdm implements Filter {
     public void destroy() {
     }
